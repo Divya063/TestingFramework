9d0a1a1df0f63f56d045f7fac439f209401b187f
Divya <ranidivya063@gmail.com>
2019-06-07 19:44:34 +0530

    includes helper function to run all the EOS tests

--- EOS/helper.py
+++ EOS/helper.py
@@ -1,21 +1,22 @@
-"""
-Helper file automate running of tests
-"""
 import os
 import glob
-def cleanup():
-    """
-    delete the created files
-    """
-    filelist = glob.glob(os.path.join("", "*.txt"))
-    for f in filelist:
-        os.remove(f)
+from test_throughput import Benchmark
+from test_checksum import Corruption
 
-def run_eos(tests, tasks):
-    if(len(tests)==1):
-        print(tests)
+
+def run_eos(tasks):
+    """
+     Helper function for running EOS test suite
+    """
     test_storage = tasks['tests']['storage']
     number_of_files = test_storage['throughput']['fileNumber']
     size = test_storage['throughput']['fileSize']
-    # test = Benchmark(number_of_files, size)
-    logger.info(test.print_result())
+    exit_code = 0
+    test_io = Benchmark(number_of_files, size)
+    print("Throughput Test\n exit code:%s "% (test_io.exit_code()))
+    exit_code |= test_io.exit_code()
+    test_integrity= Corruption()
+    print("Integrity Test\n exit code:%s " % (test_integrity.exit_code()))
+    exit_code |= test_integrity.exit_code()
+    return exit_code
+
