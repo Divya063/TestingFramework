8125ea65102362ac4fc9d0c1ffecb561c96c2cb3
Divya <ranidivya063@gmail.com>
2019-06-15 07:45:40 +0530

    validator function

--- run.py
+++ run.py
@@ -27,6 +27,46 @@ def get_config(cfg):
         raise Exception("yaml file not present")
         sys.exit()
 
+def check_test_exists(directory, test_name):
+    """
+    Checks if mentioned tests exist in particular directory or not
+    """
+    directory_path = os.path.join(os.getcwd(), 'tests') #name of directory under which test exists
+    if(test_name!= "statFile"):
+        test_file = "test_" + test_name + ".py"
+        test_file_path = os.path.join(os.path.join(directory_path, directory), test_file)
+        if not os.path.exists(test_file_path):
+            raise Exception( test_name + " test file does not exists")
+
+def check_input_validity(params):
+    """
+    Check the parameter type
+    """
+    for key , value in params.items():
+        if "number" in key.lower():
+            if not value.is_integer():
+                raise Exception(str(value)+ "is not integer")
+        if "size" in key.ilower():
+            if not type(value)==str:
+                raise Exception( str(value) + "is not string")
+
+
+
+
+def validator(tasks):
+    """
+    To check validity of YAML File
+    """
+    for test in tasks.values():
+        for directory, component_test in test.items(): #storage
+            if(component_test!=None):
+                for test_name, param in component_test.items():
+                    if(param!=None):
+                        check_test_exists(directory, test_name)
+                        check_input_validity(param)
+
+
+
 def cleanup():
     """
     delete the created files
@@ -40,6 +80,8 @@ def main():
     args = get_args()
     yaml_path= os.path.join(os.getcwd(), args.configfile)
     tasks = get_config(yaml_path)
+   # Validates YAML File
+    validator(tasks)
     for test in args.test:
         if test == "EOS":
             #passes the parameters loaded from yaml file to helper function
