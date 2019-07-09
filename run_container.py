6c157c30bfb214897dfe15b940376b2be0f64692
Divya <ranidivya063@gmail.com>
2019-07-09 12:28:59 +0530

    added support to run from user-container

---
+++ run_container.py
@@ -0,0 +1,39 @@
+import argparse
+import glob
+import os
+import sys
+import yaml
+import subprocess
+"""
+run tests from user container
+"""
+from tests.storage.helper import run_storage
+from tests.jupyterhub_api.helper import run_jupyterhub_api
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Arguments', formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('-t', '--test',
+                        nargs='*', default='compTest',
+                        required=True,
+                        help='name of the test you want to run')
+    args = parser.parse_args()
+    return args
+
+def get_config(cfg):
+    if os.path.exists(cfg):
+        with open(cfg, 'r') as stream:
+            return yaml.safe_load(stream)
+    else:
+        raise Exception("yaml file not present")
+        sys.exit()
+
+def main():
+    tasks = get_config('test.yaml')
+    args = get_args()
+    if(args.test[0]=="storage"):
+        run_storage(tasks)
+    if (args.test[0]) == "jupyterhub-api":
+        run_jupyterhub_api(tasks)
+
+if __name__ == "__main__":
+    main()
