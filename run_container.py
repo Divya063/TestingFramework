d36d6ac2bea7c524c7153d4e4c1a515bba2d9754
Divya <ranidivya063@gmail.com>
2019-07-22 08:54:09 +0530

    reformating

--- run_container.py
+++ run_container.py
@@ -4,21 +4,24 @@ import os
 import sys
 import yaml
 import subprocess
+
 """
 run tests from user container
 """
 from tests.storage.helper import run_storage
-#from tests.jupyterhub_api.helper import run_jupyterhub_api
+from tests.jupyterhub_api.helper import run_jupyterhub_api
 from tests.cvmfs.helper import run_cvmfs
 
+
 def get_args():
-    parser = argparse.ArgumentParser(description='Arguments', formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(description='Arguments', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-t', '--test',
                         nargs='*', default='compTest',
                         required=True,
                         help='name of the test you want to run')
     args = parser.parse_args()
     return args
+
 
 def get_config(cfg):
     if os.path.exists(cfg):
@@ -28,16 +31,17 @@ def get_config(cfg):
         raise Exception("yaml file not present")
         sys.exit()
 
+
 def main():
     tasks = get_config('test.yaml')
     args = get_args()
-    if(args.test[0]=="storage"):
+    if args.test[0] == "storage":
         run_storage(tasks)
     if (args.test[0]) == "jupyterhub-api":
         run_jupyterhub_api(tasks)
-
     if (args.test[0]) == "CVMFS":
         run_cvmfs(tasks)
+
 
 if __name__ == "__main__":
     main()
