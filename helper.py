bbe842a627cd553f8491b47285d1957d5123f3b4
Divya <ranidivya063@gmail.com>
2019-07-22 09:29:40 +0530

    removed concatenation

--- helper.py
+++ helper.py
@@ -1,14 +1,13 @@
-"""
+# Copies files and folders to relevant containers
 
-Copies files and folders to relevant containers
-"""
 import subprocess
 import os
+
 
 def cp_helper(name, test_name):
     container_name = "jupyter-" + name
     if test_name == "storage":
-        dest= "scratch/" + name +"/"
+        dest = "scratch/" + name + "/"
         docker_cp_host(container_name, dest)
     elif test_name == "jupyterhub-api":
         container_name = "jupyterhub"
@@ -17,21 +16,18 @@ def cp_helper(name, test_name):
         docker_cp_host(container_name, "")
 
 
-
 def docker_cp_host(container_name, dest):
     file_list = ['run.py', 'test.yaml', 'run_container.py']
     folder_list = ['tests']
     for file in file_list:
         try:
-            cmd = "docker cp " + file + " " + container_name+ ":/" + dest + file
+            cmd = "docker cp %s %s:/%s%s" % (file, container_name, dest, file)
             os.system(cmd)
         except Exception as exc:
             raise Exception
     for folder in folder_list:
         try:
-            cmd = "docker cp " + folder +"/." " " + container_name + ":/" + dest + folder
+            cmd = "docker cp %s/. %s:/%s%s" % (folder, container_name, dest, folder)
             os.system(cmd)
         except Exception as exc:
             raise Exception
-
-
