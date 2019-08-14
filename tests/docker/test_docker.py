a3abf3b543acf2130dce245f1d75630f78efc0e0
Divya <ranidivya063@gmail.com>
2019-08-14 14:24:24 +0530

    added required comments

--- tests/docker/test_docker.py
+++ tests/docker/test_docker.py
@@ -9,7 +9,9 @@ import docker
 sys.path.append("..")
 import time
 from logger import Logger, LOG_FOLDER, LOG_EXTENSION
+import threading
 from TestBase import Test
+import _thread as thread
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -21,6 +23,9 @@ def get_args():
     parser.add_argument("--name", dest="name",
                         required=False,
                         help='name')
+    parser.add_argument("--timeout", type=int,
+                        required=False,
+                        help='timeout')
 
     args = parser.parse_args()
     return args
@@ -32,29 +37,45 @@ client = docker.client.from_env()
 class Docker(Test):
     """Test to check if user container is healthy"""
 
-    def __init__(self, container_name):
+    def __init__(self, container_name, timeout):
+        self.timeout = timeout
         self.ref_test_name = "container"
         self.container_name = container_name
         super().__init__()
 
+    def quit_function(self, fn_name):
+        self.log.write("error", '{0} took too long'.format(fn_name))
+        exit(1)
+
     def check_container(self):
+        timer = threading.Timer(self.timeout, self.quit_function, args=['check_container'])
+        timer.start()
         # start docker events
-        events = client.events(decode=True)
-        # exec into container and run a command
-        output = client.containers.get(self.container_name).exec_run(cmd=["bash", "-c", "ls -A "], workdir="/",
-                                                                     stdout=True)
-        exit_code = output[0]
-        msg = output[1].decode("utf-8")
-        status_list = ['exec_create', 'exec_start', 'exec_die']
-        event_number = 0
-        for event in events:
-            self.log.write("info", str(event))
-            # Checks if given status (given in the status_list) is present (inside events) or not
-            if event['status'] != status_list[event_number] and not event['id']:
-                return 1
-            event_number = event_number + 1
-            if event_number == 3:
-                break
+        try:
+            events = client.events(decode=True)
+            # exec into container and run a command
+            output = client.containers.get(self.container_name).exec_run(cmd=["bi/ls", "-A"], workdir="/",
+                                                                         stdout=True)
+        except Exception as exc:
+            self.log.write("error", str(exc))
+            return 1
+        else:
+            self.log.write("info", str(output))
+            exit_code = output[0]
+            msg = output[1].decode("utf-8")
+            # On a successful exec there would be three events, if exec failed,
+            # status "exec_die" would not appear, i.e. total events would be less than 3
+            status_list = ['exec_create', 'exec_start', 'exec_die']
+            for event_number, event in enumerate(events):
+                self.log.write("info", str(event))
+                # Checks if given status (given in the status_list) is present (inside events) or not
+                if event['status'] != status_list[event_number] and not event['id']:
+                    return 1
+                if event_number == 2:
+                    # to stop docker events, otherwise it would continue to listen
+                    break
+        finally:
+            timer.cancel()
 
         self.log.write("info", "exit code :" + str(exit_code))
         return exit_code
@@ -62,5 +83,5 @@ class Docker(Test):
 
 if __name__ == "__main__":
     args = get_args()
-    test_container = Docker(args.name)
+    test_container = Docker(args.name, args.timeout)
     (test_container.check_container())
