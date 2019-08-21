76a508197d283661689442e13ca7f6f2e9a26a82
Divya <ranidivya063@gmail.com>
2019-08-21 09:38:27 +0530

    improved mount check

--- tests/storage/test_mount.py
+++ tests/storage/test_mount.py
@@ -8,8 +8,9 @@ import _thread as thread
 import os
 from logger import Logger, LOG_FOLDER, LOG_EXTENSION
 from TestBase import Test
+import re
+import urllib
 import argparse
-
 
 class Mount(Test):
     """Checks for eos mount points"""
@@ -22,15 +23,36 @@ class Mount(Test):
         olddir = os.getcwd()
         os.chdir('/')
         first_command = subprocess.Popen(["mount", "-l"], stdout=subprocess.PIPE)
-        second_command = subprocess.Popen(["grep", "eos"], stdin=first_command.stdout)
-        if second_command:
-            self.log.write("sanity", "eos mount points" + " exist")
-            os.chdir(olddir)
-            return 0
-        else:
+        out, err = first_command.communicate()
+        regex = re.compile(r'\b(eos.*on.*type.*fuse.*)\b', re.MULTILINE)
+        regex_second = re.compile(r'\b(home-.*on*type.fuse.*)\b', re.MULTILINE)
+        regex_docker_mount = re.compile(r'\b(eosdocker.*on.*/eos/.*type.*fuse)\b', re.MULTILINE)
+
+        # On sciencebox , mount -l | grep eos resulted in
+        # /dev/sda5 on /tmp/sciencebox/eos_mount type ext4 (rw,relatime,errors=remount-ro,stripe=32699,data=ordered)
+
+        regex_docker_mount_second = re.compile(r'\b(.*/tmp/sciencebox/eos_mount.*type.*)\b', re.MULTILINE)
+        out = out.decode('utf-8')
+        result = regex.findall(out)
+        result_host = regex_second.findall(out)
+        result_docker = regex_docker_mount.findall(out)
+        result_docker_second = regex_docker_mount_second.findall(out)
+
+        # Checks for result from host and sciencebox, if any of the result is true return true
+        exit_code = 1
+        if result and result_host:
+            self.log.write("sanity", "eos mount points" + " exist on host")
+            exit_code = 0
+        if result_docker or result_docker_second:
+            self.log.write("sanity", "[sciencebox] eos mount points" + " exist")
+            exit_code = 0
+
+        os.chdir(olddir)
+
+        if exit_code:
             self.log.write("error", "eos mount points" + " do not exist")
             return 1
-
+        return 0
 
     def exit_code(self):
         self.exit = self.check_mount()
